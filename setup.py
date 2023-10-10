dcb3a68fdcb2d3e502c42f6aa34f64146d0aed05
Mohamed Ali <01marzouq@gmail.com>
2023-10-10 16:01:56 +0300

    updating setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="secureDataTransfer",
-    version="0.1.1",
+    version="0.1.0",
     description="The 'Secure Data Transfer'  seamlessly is a robust solution for safeguarding and sharing sensitive information.",
     packages=find_packages(),
     install_requires=[
