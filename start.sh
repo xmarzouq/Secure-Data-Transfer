648dce4046752f3797c0cf3f1004bdb0d4ba29c2
Mohamed Ali <01marzouq@gmail.com>
2023-10-16 10:31:40 +0300

    deleting start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-pip3 install virtualenv
-python3 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-echo "------------------------------------------------"
-python3 utils/keyPairGenerator.py
-# python3 mainGUI.py
